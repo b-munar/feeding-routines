2c2e7c7fafa53bc2512999a0d5b6a92f7b21fe5f
b-munar <b.munar@uniandes.edu.co>
2024-03-30 21:02:51 -0500

    improve for authenticator integration

--- src/schemas/note_schema.py
+++ src/schemas/note_schema.py
@@ -4,6 +4,7 @@ class NoteDeserializeSchema(Schema):
     note = fields.String(required=True)
 
 class NoteSerializeSchema(Schema):
+    id = fields.UUID()
     note = fields.String()
     updateAt = fields.DateTime(format='%Y-%m-%dT%H:%M:%S%z')
     createdAt = fields.DateTime(format='%Y-%m-%dT%H:%M:%S%z')
